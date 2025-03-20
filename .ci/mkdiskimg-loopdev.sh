a4e5939adac1f237389a28650a901c0b2fa7c330
ChinYikMing <yikming2222@gmail.com>
2025-03-20 15:45:33 +0800

    test

--- .ci/mkdiskimg-loopdev.sh
+++ .ci/mkdiskimg-loopdev.sh
@@ -41,7 +41,7 @@ case "$ACTION" in
         chmod o+r,o+w ${BLK_DEV}
 
         # Export ${BLK_DEV} to a tmp file. Then, export to "$GITHUB_ENV" in job step.
-        echo "BLK_DEV=${BLK_DEV}" | sudo tee "${TMP_FILE}"
+        echo "BLK_DEV=${BLK_DEV}" > "${TMP_FILE}"
         ;;
     delete)
         # Detach the /dev/loopx(Linux) or /dev/diskx(Darwin) to release system resources
