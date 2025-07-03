74602dd32bd25b2b8e77c7c978251f561629f48b
ChinYikMing <yikming2222@gmail.com>
2025-07-04 04:52:34 +0800

    Use npx prettier for linting

--- assets/wasm/js/pre.js
+++ assets/wasm/js/pre.js
@@ -1,78 +1,75 @@
-Module['noInitialRun'] = true;
+Module["noInitialRun"] = true;
 let init = false;
 let cbuffer_ptr;
 let term;
 let sequence;
 
-Module['onRuntimeInitialized'] = function(target_elf) {
-	if(!init){
-		init = true;
-	cbuffer_ptr = Module._get_input_buffer();
+Module["onRuntimeInitialized"] = function (target_elf) {
+  if (!init) {
+    init = true;
+    cbuffer_ptr = Module._get_input_buffer();
+    cbuffer_cap = Module._get_input_buffer_cap();
 
     term = new Terminal({
-        cols: 120,
-        rows: 25,
+      cols: 120,
+      rows: 25,
     });
-    term.open(document.getElementById('terminal'));
-    term.write('$ ');
-    term.scrollToBottom();
-
-    let currLine = "";
+    term.open(document.getElementById("terminal"));
 
     term.onKey(({ key, domEvent }) => {
-        const code = key.charCodeAt(0);
+      const code = key.charCodeAt(0);
 
-	switch (domEvent.key) {
-        case 'ArrowUp':
-            // Real terminal sends ESC [ A → "\x1B[A"
-	    sequence = '\x1B[A';
-            break;
-        case 'ArrowDown':
-            // Real terminal sends ESC [ B → "\x1B[B"
-	    sequence = '\x1B[B';
-            break;
-        case 'ArrowRight':
-            // Real terminal sends ESC [ C → "\x1B[C"
-	    sequence = '\x1B[C';
-            break;
-        case 'ArrowLeft':
-            // Real terminal sends ESC [ D → "\x1B[D"
-	    sequence = '\x1B[D';
-            break;
-	// TODO: support more keys?
+      switch (domEvent.key) {
+        case "ArrowUp":
+          // ESC [ A → "\x1B[A"
+          sequence = "\x1B[A";
+          break;
+        case "ArrowDown":
+          // ESC [ B → "\x1B[B"
+          sequence = "\x1B[B";
+          break;
+        case "ArrowRight":
+          // ESC [ C → "\x1B[C"
+          sequence = "\x1B[C";
+          break;
+        case "ArrowLeft":
+          // ESC [ D → "\x1B[D"
+          sequence = "\x1B[D";
+          break;
+        // TODO: support more keys?
         default:
-	    sequence = key;
-	    break;
-    }
+          sequence = key;
+          break;
+      }
 
-            let heap = new Uint8Array(Module.HEAPU8.buffer, cbuffer_ptr, sequence.length);
+      let heap = new Uint8Array(
+        Module.HEAPU8.buffer,
+        cbuffer_ptr,
+        sequence.length,
+      );
 
-	    for (let i = 0; i < sequence.length && i < 8; i++) {
-                heap[i] = sequence.charCodeAt(i);
-            }
-            for (let i = sequence.length; i < 8; i++) {
-                heap[i] = 0;  // Zero-fill
-            }
+      for (let i = 0; i < sequence.length && i < cbuffer_cap; i++) {
+        heap[i] = sequence.charCodeAt(i);
+      }
+      for (let i = sequence.length; i < cbuffer_cap; i++) {
+        heap[i] = 0; // Zero-fill
+      }
 
-	    Module._set_escape_char_size(sequence.length);
-            Module._set_input_buffer_in(true);
+      Module._set_escape_char_size(sequence.length);
+      Module._set_input_buffer_in(true);
 
-	//console.log(code);
-
-        term.scrollToBottom();
+      term.scrollToBottom();
     });
+  }
 
-	}
+  if (target_elf === undefined) {
+    console.warn("target elf executable is undefined");
+    return;
+  }
 
-
-    if(target_elf === undefined){
-      console.warn("target elf executable is undefined");
-      return;
-    }
-
-    if(target_elf.startsWith("-k")){
-        callMain(target_elf.split(" "));
-    } else {
-        callMain([target_elf]);
-    }
+  if (target_elf.startsWith("-k")) {
+    callMain(target_elf.split(" "));
+  } else {
+    callMain([target_elf]);
+  }
 };
