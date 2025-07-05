77695cf667125e8cd43d1e81c30ff763d9109a15
ChinYikMing <yikming2222@gmail.com>
2025-07-05 22:31:37 +0800

    Add shareware Doom IWAD

--- src/riscv.c
+++ src/riscv.c
@@ -702,7 +702,7 @@ void rv_run(riscv_t *rv)
     );
 
     if (!(attr->run_flag & (RV_RUN_TRACE | RV_RUN_GDBSTUB))) {
-	printf("1011\n");
+	printf("1011123\n");
 #ifdef __EMSCRIPTEN__
         emscripten_set_main_loop_arg(rv_step, (void *) rv, 0, 1);
 #else
