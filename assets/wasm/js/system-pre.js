0101f882d6e31b8c121c3cbc2da89e7269f3505f
ChinYikMing <yikming2222@gmail.com>
2025-07-08 22:10:37 +0800

    tmp

--- assets/wasm/js/system-pre.js
+++ assets/wasm/js/system-pre.js
@@ -18,47 +18,22 @@ Module["onRuntimeInitialized"] = function () {
   term.open(document.getElementById("terminal"));
 
   term.onKey(({ key, domEvent }) => {
-    const code = key.charCodeAt(0);
-    let sequence;
-
-    switch (domEvent.key) {
-      case "ArrowUp":
-        // ESC [ A → "\x1B[A"
-        sequence = "\x1B[A";
-        break;
-      case "ArrowDown":
-        // ESC [ B → "\x1B[B"
-        sequence = "\x1B[B";
-        break;
-      case "ArrowRight":
-        // ESC [ C → "\x1B[C"
-        sequence = "\x1B[C";
-        break;
-      case "ArrowLeft":
-        // ESC [ D → "\x1B[D"
-        sequence = "\x1B[D";
-        break;
-      // TODO: support more escape keys?
-      default:
-        sequence = key;
-        break;
-    }
 
     let heap = new Uint8Array(
       Module.HEAPU8.buffer,
       input_buf_ptr,
-      sequence.length,
+      key.length,
     );
 
-    for (let i = 0; i < sequence.length && i < input_buf_cap; i++) {
-      heap[i] = sequence.charCodeAt(i);
+    for (let i = 0; i < key.length && i < input_buf_cap; i++) {
+      heap[i] = key.charCodeAt(i);
     }
     // Fill zero
-    for (let i = sequence.length; i < input_buf_cap; i++) {
+    for (let i = key.length; i < input_buf_cap; i++) {
       heap[i] = 0;
     }
 
-    Module._set_input_buf_size(sequence.length);
+    Module._set_input_buf_size(key.length);
 
     term.scrollToBottom();
   });
