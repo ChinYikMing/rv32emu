b5b8b10ea811d8c1b3393c7ed10e4f93873be569
ChinYikMing <yikming2222@gmail.com>
2025-12-26 17:44:29 +0800

    test

--- mk/wasm.mk
+++ mk/wasm.mk
@@ -21,7 +21,7 @@ ifeq ($(call has, SDL), 1)
 # Disable STRICT mode to avoid -Werror in SDL2_mixer port compilation.
 # The emscripten-ports/SDL2_mixer was archived in Jan 2024 and has warnings
 # in music_modplug.c that become fatal errors under STRICT mode.
-CFLAGS_emcc += -sSTRICT=0 -sUSE_SDL=2 -sSDL2_MIXER_FORMATS=wav,mid -sUSE_SDL_MIXER=2
+CFLAGS_emcc += -sSTRICT=0 -sUSE_SDL=2 -sSDL2_MIXER_FORMATS=wav,mid -sUSE_SDL_MIXER=2 -pthread
 OBJS_EXT += syscall_sdl.o
 LDFLAGS += -pthread
 endif
