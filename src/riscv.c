2733801a1b27b4d78c00623a6140904aa71f9a71
ChinYikMing <yikming2222@gmail.com>
2026-02-01 17:44:37 +0800

    Refine JIT state init and T2C lifecycle for reboot
    rv_init_jit() is introduced to refresh JIT state for warm and cold
    reboots. Reboots also need explicit T2C lifecycle control if T2C is
    enabled. On reboot, rv_terminate_t2c() is called during the SBI reset
    ecall to terminate the old T2C background thread, and rv_spawn_t2c() is
    used to create a new T2C background thread.

--- src/riscv.c
+++ src/riscv.c
@@ -1095,29 +1095,7 @@ void rv_delete(riscv_t *rv)
     block_map_destroy(rv);
 #else
 #if RV32_HAS(T2C)
-    /* Signal the thread to quit */
-    pthread_mutex_lock(&rv->wait_queue_lock);
-    rv->quit = true;
-    pthread_cond_signal(&rv->wait_queue_cond);
-    pthread_mutex_unlock(&rv->wait_queue_lock);
-
-    pthread_join(t2c_thread, NULL);
-
-    /* Clean up any remaining entries in wait queue */
-    queue_entry_t *entry, *safe;
-    list_for_each_entry_safe (entry, safe, &rv->wait_queue, list) {
-        list_del(&entry->list);
-        free(entry);
-    }
-
-    pthread_mutex_destroy(&rv->wait_queue_lock);
-    pthread_mutex_destroy(&rv->cache_lock);
-    pthread_cond_destroy(&rv->wait_queue_cond);
-    jit_cache_exit(rv->jit_cache);
-    inline_cache_exit(rv->inline_cache);
-
-    /* Dispose LLVM engines for all remaining blocks before freeing cache */
-    clear_cache_hot(rv->block_cache, t2c_dispose_block_engine);
+    rv_terminate_t2c(rv);
 #endif
     /* Free branch tables for all remaining blocks before freeing cache */
     clear_cache_hot(rv->block_cache, free_block_branch_tables);
@@ -1184,6 +1162,7 @@ static void load_boot_images(vm_attr_t *attr)
         rv_log_info("Rootfs loaded (%ld bytes)", (long) initrd_size);
     }
 }
+#endif /* RV32_HAS(SYSTEM_MMIO) */
 
 #if RV32_HAS(JIT)
 /* Initialize or reinitialize JIT state.
@@ -1263,6 +1242,13 @@ void rv_warm_reboot(riscv_t *rv, riscv_word_t pc)
     /* setup RISC-V hart */
     rv_set_reg(rv, rv_reg_a0, 0);
     rv_set_reg(rv, rv_reg_a1, attr->dtb_addr);
+
+#if RV32_HAS(JIT)
+    if (!rv_init_jit(rv)) {
+        rv_log_fatal("Init JIT state at warm reboot failed");
+        exit(EXIT_FAILURE);
+    }
+#endif
 }
 #endif /* RV32_HAS(SYSTEM_MMIO) */
 
@@ -1582,18 +1568,6 @@ bool rv_cold_reboot(riscv_t *rv, riscv_word_t pc)
 
     return true;
 
-#if RV32_HAS(JIT)
-#if RV32_HAS(T2C)
-fail_inline_cache:
-    jit_cache_exit(rv->jit_cache);
-fail_jit_cache:
-    cache_free(rv->block_cache);
-#endif
-fail_block_cache:
-    if (rv->jit_state)
-        jit_state_exit(rv->jit_state);
-fail_jit_state:
-#endif
 fail_mpool:
     mpool_destroy(rv->block_ir_mp);
     mpool_destroy(rv->block_mp);
